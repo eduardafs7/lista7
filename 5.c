41bd47a207c38f90388411b011b87d495731310c
eduardafs7 <eduarda.freire@a.ucb.br>
2025-06-01 23:26:11 -0300

    Create 5.c

---
+++ 5.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    float matriz[3][3];
+
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            matriz[i][j] = (i + j) * 1.1f;
+        }
+    }
+
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            printf("Endereco do elemento [%d][%d]: %p\n", i, j, (void*)&matriz[i][j]);
+        }
+    }
+
+    return 0;
+}
